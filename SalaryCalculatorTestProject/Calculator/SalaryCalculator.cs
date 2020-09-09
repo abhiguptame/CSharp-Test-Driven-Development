859d1424668aaf1f6b617db69169e174f9685f20
abhiguptame <abhiguptame@gmail.com>
2020-09-09 17:12:45 +0530

    Refactor Code
    Refactor Code : Using Lambda Expression

--- SalaryCalculatorTestProject/Calculator/SalaryCalculator.cs
+++ SalaryCalculatorTestProject/Calculator/SalaryCalculator.cs
@@ -6,16 +6,9 @@ namespace Calculator
     {
         const int HoursInYear = 2080;
 
-        public decimal GetAnnualSalary(decimal hourlyWage)
-        {
-            decimal annualSalary = hourlyWage * HoursInYear;
-            return annualSalary;
-        }
-
-        public decimal GetHourlyWage(decimal annualSalary)
-        {
-            decimal hourlyWage = annualSalary / HoursInYear;
-            return hourlyWage;
-        }
+        public decimal GetAnnualSalary(decimal hourlyWage) => hourlyWage * HoursInYear;
+  
+        public decimal GetHourlyWage(decimal annualSalary) => annualSalary / HoursInYear;
+       
     }
 }
